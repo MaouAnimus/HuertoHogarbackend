5440c603088c966fc445754dcdef81e8eb866741
Maou <166620832+MaouAnimus@users.noreply.github.com>
2025-10-06 14:01:54 -0300

    Add files via upload

---
+++ src/main/java/com/huerto_hogar/huertoh/HuertohApplication.java
@@ -0,0 +1,13 @@
+package com.huerto_hogar.huertoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuertohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuertohApplication.class, args);
+	}
+
+}
